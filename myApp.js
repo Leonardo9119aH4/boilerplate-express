451eca94ea39051627c8865ab1c0ea9501761652
Leonardo Artur Sasse Maia Lopes <leonardoartursasse@gmail.com>
2025-03-21 14:42:52 +0000

    exercicios

--- myApp.js
+++ myApp.js
@@ -1,6 +1,19 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
 
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname+"/views/index.html");
+});
+app.use("/public", express.static(__dirname+"/public"));
+app.get("/json", (req, res) => {
+    if(process.env.MESSAGE_STYLE == "uppercase"){
+        res.json({"message": "HELLO JSON"});
+    }
+    else{
+        res.json({"message": "Hello json"});
+    }
+})
 
 
 
